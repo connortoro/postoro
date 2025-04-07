187467d4a0522703d4d1d4c3dbc34216dba5c464
Connor Toro <connortoro@Connors-Air-2.csusm.edu>
2025-04-07 15:30:15 -0700

    Admin colors

--- app/components/user-tag.tsx
+++ app/components/user-tag.tsx
@@ -18,10 +18,12 @@ export default function UserTag({username, pic} : UserTagProps) {
     redirect(`/app/profile/${username}`)
   }
 
+  const color = username == 'connor' ? 'bg-gradient-to-r from-green-200 to-blue-200 bg-clip-text text-transparent' : 'text-white'
+
   return(
     <div onClick={(e) => handleClick(e)} className="flex flex-row items-center justify-center space-x-2">
       <Image src={pic || blank_pfp} height={30} width={30} alt="pfp" className='rounded-full'></Image>
-      <h3 className='font-bold hover:underline hover:cursor-pointer text-white'>{username}</h3>
+      <h3 className={'font-bold hover:underline hover:cursor-pointer ' + color}>{username}</h3>
     </div>
   )
 }
